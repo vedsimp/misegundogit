e69a98f16ce5a9c5341bc05dd3d8501c15c3f8f2
vedsimp <bettsygarces12@gmail.com>
2021-09-07 14:27:35 -0500

    nada

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+#Este proyecto fue creado el 7 de septiermbre del 2021 por Bettsy Garces
+#Este proyecto el pregunta al usuario acerca de sus gustos musicales 
+def gustosmusicales():
+    print("Hola, cuentame que genero de musica te gusta mas?")
+    gener = input()
+    print("Oh,",gener,"y cual es tu cancion favorita?")
+    cancion = input()
+    print("uuh!",cancion, "es buena")
+
+gustosmusicales()
